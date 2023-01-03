6ce62338cfd78f128d9d7478cd120babce6657f7
kkyu0718 <kyuwon0718@gmail.com>
2023-01-04 04:27:01 +0900

    [feat] content/
    게시물 전체 조회 구현 완료

--- src/router/contentRouter.ts
+++ src/router/contentRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { contentController } from "../controller";
+import { body } from "express-validator"
 
 const router: Router = Router();
 
 //* 컨텐츠 전체 조회 - 다날 정보 ( GET /content )
-router.get("/", contentController.getAllContent);
+router.get("/", body("user_key").notEmpty(), contentController.getAllContent);
 
 export default router;
--- src/service/contentService.ts
+++ src/service/contentService.ts
@@ -5,20 +5,49 @@ const prisma = new PrismaClient();
 
 //* 컨텐츠 전체 조회 - 사용자의 좋아요 여부 포함
 const getAllContent = async (user_key: number) => {
-  const data: AllContentsDto[] =
-    await prisma.$queryRawUnsafe(`select A.content_key, A.content_title, A.introduction, A.start_at, A.end_at, D.category_name as category,
-    case when b.liked_key is not null
-        then true
-        else false
- end as liked
-from "Contents" A
-left join "Liked" B
-on (A.content_key = B.content) and (B.user = ${user_key})
-left join "ContentMapping" C
-on A.content_key = C.content
-left join "ContentCategory" D
-on C.category = D.category_key;
-`);
+  const user = await prisma.user.findUnique({
+    where: {
+      user_key: user_key,
+    },
+  });
+  const { user_character }: number = user;
+
+  const { inter } :string = await prisma.characters.findUnique({
+    where: {
+      character_key: user_character,
+    },
+  });
+
+  const data: AllContentsDto[] = await prisma.$queryRawUnsafe(
+    `select content_key, content_title, introduction, start_at, end_at, category, liked
+    from (select *, case
+    when temp.interest = '${inter}'
+    then 0
+    else 1
+    end as priority
+    from (select content_key, content_title, introduction, start_at, end_at, category, unnest(T.interest) as interest, liked from (select A.content_key, A.content_title, A.introduction, A.start_at, A.end_at,
+                      array_agg(D.category_name) as category,
+                      coalesce(array_agg(D.inter) Filter ( Where D.inter != '혜택' ), null) as interest,
+            case
+                when b.liked_key is not null
+                then true
+                else false
+            end as liked,
+            rank() over (Partition By coalesce(array_agg(D.inter) Filter ( Where D.inter != '혜택' ), null) ORDER BY content_key ASC) as a
+
+        from "Contents" A
+        left join "Liked" B
+        on (A.content_key = B.content) and (B.user = ${user_key})
+        left join "ContentMapping" C
+        on A.content_key = C.content
+        left join "ContentCategory" D
+        on C.category = D.category_key
+        group by A.content_key, b.liked_key) as T
+    where T.a <= 3) as temp) as result
+    order by priority ASC
+    ;`
+  );
+
   return data;
 };
 
