9fe2ebeb06e96dc1e5ebe2a5952cd87570de351a
kkyu0718 <kyuwon0718@gmail.com>
2023-01-09 21:25:03 +0900

    [fix] content type fixed

--- src/interfaces/content/AllContentsDTO.ts
+++ src/interfaces/content/AllContentsDTO.ts
@@ -1,9 +1,9 @@
 export interface AllContentsDto {
   content_key: number;
   content_title: string;
-  introduction: string;
+  introduction?: string;
   category: string[];
-  start_at: string;
-  end_at: string;
+  start_at?: string;
+  end_at?: string;
   liked: boolean;
 }
